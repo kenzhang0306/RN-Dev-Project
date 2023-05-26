86c37061e329a367848cde5c80672a1237cbcf4f
kenzhang0306 <it@mainstream.co.nz>
2023-05-26 21:31:43 +1200

    new file:   components/HeaderButton.js 	modified:   package-lock.json 	modified:   package.json 	modified:   screens/MealDetailScreen.js

---
+++ components/HeaderButton.js
@@ -0,0 +1,18 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { HeaderButton } from "react-navigation-header-buttons";
+
+export default function CustomHeaderButton(props) {
+  return (
+    <View style={{ marginRight: 10 }}>
+      <HeaderButton
+        {...props}
+        iconSize={23} // Optional, specify the icon size
+        color="white" // Optional, specify the icon color
+        IconComponent={Ionicons} // Optional, specify a custom icon component
+        iconProps={{ style: { marginBottom: 2 } }} // Optional, specify custom icon props
+      />
+    </View>
+  );
+}
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,6 +1,14 @@
-import { View, Text, Button, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TouchableNativeFeedback,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
+import CustomHeaderButton from "../components/HeaderButton";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 export default function MealDetailScreen(props) {
   const route = useRoute();
@@ -8,9 +16,34 @@ export default function MealDetailScreen(props) {
   const ingredients = route.params?.ItemDetails.ingredients;
   const navigation = useNavigation();
 
+  const renderHeaderRight = () => {
+    return (
+      // <TouchableNativeFeedback
+      //   onPress={() => {
+      //     //TODO
+      //     props.navigation.popToTop();
+      //   }}
+      // >
+      //   <Text style={{ marginRight: 10 }}>Custom Button</Text>
+      // </TouchableNativeFeedback>
+
+      //you can add multiple items
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="favorite"
+          iconName="ios-star"
+          onPress={() => {
+            // Handle button press here
+          }}
+        />
+      </HeaderButtons>
+    );
+  };
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTitle: title,
+      headerRight: renderHeaderRight,
     });
   }, [navigation]);
 
